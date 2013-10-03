4cd5928a2837106574a6243c10204bb5a0a7cfc9
Frank <frankbasti@outlook.com>
2013-10-03 04:47:37 -0700

    nonsense

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Frank Basti - In Loving Memory</title>
+<meta name="description" content=" Born 1979, Died 2012. He loved cats, but hated overweight women.">
+<link rel="shortcut icon" href="/x/f/g/ico/nx.ico" type="image/x-icon" />
+<link rel="image_src" type="image/jpeg" href="http://frankbasti.com/fb.png" />
+<meta property="og:image" content="http://frankbasti.com/fb.png" />
+<style>
+body{background:#1a4f7c; margin:0px; padding:0px} #main {display: block; width: 1000px; height: 1000px; margin: auto; background:url('frank-basti.jpg')} #memory{font-size:10px; color:#1a4f7c; display: block; margin; auto; width: 100%; text-align: center; font-family: "Lucida Console", Monaco, monospace}
+</style>
+</head>
+<body>
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=217741161681780";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+<div id="main"></div>
+<div id="memory">
+	<p>In loving Memory of Frank Basti</p>
+	<p> Born 1979, Died 2013</p>
+	<p>He liked cats but hated overweight women</p>
+	<div class="fb-like" data-href="http://frankbasti.com" data-width="The pixel width of the plugin" data-height="The pixel height of the plugin" data-colorscheme="light" data-layout="button_count" data-action="like" data-show-faces="false" data-send="false"></div>
+<div>
+<audio autoplay="autoplay" controls="controls" hidden="true">  
+	<source src="pop.mp3" />  
+</audio>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+<script type="text/javascript">
+$(document).ready(function () {
+$('#main').hide().delay(0).fadeIn(2500);
+});
+</script>
+</body>
+</html>
