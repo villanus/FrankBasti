cd6e42e21ac6f8f0d9bf60b28059a37908bfeca2
Frank <frankbasti@outlook.com>
2014-04-30 05:14:00 -0700

    stupid space

--- notify-error.php
+++ notify-error.php
@@ -6,7 +6,7 @@ MailJet Using frankbasti@gmail.com account..
 */
 
 header('Access-Control-Allow-Origin: *');  
-$cmdstart = '/usr/bin/curl --user "e5b7e3b71ac9760589a398e60b5a02f5:17fc6b1d1eac8eb019605505bcf19d6b" http://api.mailjet.com/v3/send -F from="site-errors@decalicious.com" -F to="site-error-500@decalicious.com" -F subject="Decalicious.com Website Error" -F text=" ';
+$cmdstart = '/usr/bin/curl --user "e5b7e3b71ac9760589a398e60b5a02f5:17fc6b1d1eac8eb019605505bcf19d6b" http://api.mailjet.com/v3/send -F from="site-errors@decalicious.com" -F to="site-error-500@decalicious.com" -F subject="Decalicious.com Website Error" -F text="';
 $cmdend = ' "';
 $msg = $_GET["msg"];
 $cmd = $cmdstart . $msg . $cmdend;
