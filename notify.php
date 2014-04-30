684873a459e46dc8eb6391469affe4dc2a3782a7
Frank <frankbasti@outlook.com>
2014-04-30 04:19:52 -0700

    Notify Error

--- notify.php
+++ notify.php
@@ -1,12 +1,12 @@
 <?php
 
-$ch = curl_init("http://www.example.com/");
-$fp = fopen("example_homepage.txt", "w");
+echo exec('whoami');
 
-curl_setopt($ch, CURLOPT_FILE, $fp);
-curl_setopt($ch, CURLOPT_HEADER, 0);
 
-curl_exec($ch);
-curl_close($ch);
-fclose($fp);
+$cmd = '/usr/bin/curl --user "e5b7e3b71ac9760589a398e60b5a02f5:17fc6b1d1eac8eb019605505bcf19d6b" http://api.mailjet.com/v3/send -F from="site-errors@decalicious.com" -F to="site-error-500@decalicious.com" -F subject="Decalicious - HTTP ERROR" -F text="Greetings from the city of lights."';
+echo ($cmd);
+exec($cmd);
+echo ('done');
+
 ?>
+
